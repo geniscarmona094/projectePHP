aa3da0bbd620070fccb7e0bf209680c587d4fe86
geniscarmona094 <genis.carmonat@educem.net>
2023-01-18 10:17:52 +0100

    script bd

--- db/database.sql
+++ db/database.sql
@@ -1,1 +1,17 @@
 -- PUT YOUR SQL SCHEMA CODE HERE 
+
+CREATE DATABASE IF NOT EXISTS `isitec` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+USE `isitec`;
+
+CREATE TABLE IF NOT EXISTS users(
+    `iduser` INT AUTO_INCREMENT , 
+    `mail` VARCHAR(40) NOT NULL , 
+    `username` VARCHAR(16) NOT NULL , 
+    `passHash` VARCHAR(60) NOT NULL , 
+    `userFirstName` VARCHAR(60) NOT NULL , 
+    `userLastName` VARCHAR(120) NOT NULL , 
+    `creationDate` DATETIME NOT NULL , 
+    `removeDate` DATETIME NOT NULL , 
+    `lastSignIn` DATETIME NOT NULL , 
+    `active` TINYINT(1) NOT NULL , 
+    PRIMARY KEY (`iduser`), UNIQUE (`username`), UNIQUE (`mail`)) ENGINE = InnoDB;
